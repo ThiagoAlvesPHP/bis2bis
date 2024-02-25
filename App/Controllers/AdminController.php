d1eb06276107b265573e51df6f038ad2fe532ed6
ThiagoAlvesPHP <thiagoalves@ltdeveloper.com.br>
2024-02-25 20:09:52 -0300

    inclusão de contagem de usuarios e posts mais difinição de data, hora e dia da semana  em home do dashboard

--- App/Controllers/AdminController.php
+++ App/Controllers/AdminController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Models\PostModel;
+use App\Models\UserModel;
+
 class AdminController extends BaseController
 {
+    private $UserModel;
     public $title = "Dashboard";
     public $page = "dashboard";
+    public $countUsers = 0;
+    public $countPosts = 0;
 
-    public function __construct()
+    public function __construct($db)
     {
         parent::__construct();
+        $this->countUsers = count((new UserModel($db))->getAll());
+        $this->countPosts = count((new PostModel($db))->getAll());
     }
 
     public function index()
--- App/views/pages/dashboard.php
+++ App/views/pages/dashboard.php
@@ -10,12 +10,12 @@
     <div class="col">
         <div class="card">
             <div class="card-header">
-                Usuários
+                <i class="fas fa-users"></i> Usuários
             </div>
             <div class="card-body">
-                <h5 class="card-title">X</h5>
+                <h5 class="card-title"><?= $this->countUsers; ?></h5>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <a href="#" class="btn btn-primary">
+                    <a href="<?= BASE; ?>admin/users" class="btn btn-primary">
                         <i class="fas fa-location-arrow"></i>
                     </a>
                 </div>
@@ -25,12 +25,12 @@
     <div class="col">
         <div class="card">
             <div class="card-header">
-                Usuários
+                <i class="fas fa-address-card"></i> Posts
             </div>
             <div class="card-body">
-                <h5 class="card-title">X</h5>
+                <h5 class="card-title"><?= $this->countPosts; ?></h5>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <a href="#" class="btn btn-primary">
+                    <a href="<?= BASE; ?>admin/posts" class="btn btn-primary">
                         <i class="fas fa-location-arrow"></i>
                     </a>
                 </div>
@@ -43,13 +43,34 @@
                 <?= date('d/m/Y'); ?>
             </div>
             <div class="card-body">
-                <h5 class="card-title">X</h5>
+                <h5 class="card-title" id="time">X</h5>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <a href="#" class="btn btn-primary">
-                        <i class="fas fa-location-arrow"></i>
-                    </a>
+                    <?=date('l', strtotime(date('Y-m-d'))) ?>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    // Função para atualizar a hora no elemento HTML
+    function updateTime() {
+        // Obtenha a data e hora atual
+        var now = new Date();
+
+        // Extraia horas, minutos e segundos
+        var hours = ('0' + now.getHours()).slice(-2);
+        var minutes = ('0' + now.getMinutes()).slice(-2);
+        var seconds = ('0' + now.getSeconds()).slice(-2);
+
+        // Formate a hora como "hh:mm:ss"
+        var formattedTime = hours + ':' + minutes + ':' + seconds;
+
+        // Atualize o conteúdo do elemento HTML
+        $('#time').text(formattedTime);
+    }
+    // Atualize a hora a cada segundo
+    setInterval(updateTime, 1000);
+    // Chame a função inicialmente para exibir a hora imediatamente
+    updateTime();
+</script>
